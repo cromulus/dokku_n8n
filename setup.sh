3c90460a9b0a7ee98f7afa0e016d15679874c7b3
Bill Cromie <bill.cromie@gmail.com>
2023-08-07 17:04:24 -0400

    removing P

--- setup.sh
+++ setup.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 if [[ -z "${DB_POSTGRESDB_HOST}" ]]; then
-DB_POSTGRESDB_USER=$(echo $DATABASE_URL | grep -oP "postgres://\K(.+?):" | cut -d: -f1)
-DB_POSTGRESDB_PASSWORD=$(echo $DATABASE_URL | grep -oP "postgres://.*:\K(.+?)@" | cut -d@ -f1)
-DB_POSTGRESDB_HOST=$(echo $DATABASE_URL | grep -oP "postgres://.*@\K(.+?):" | cut -d: -f1)
-DB_POSTGRESDB_PORT=$(echo $DATABASE_URL | grep -oP "postgres://.*@.*:\K(\d+)/" | cut -d/ -f1)
-DB_POSTGRESDB_DATABASE=$(echo $DATABASE_URL | grep -oP "postgres://.*@.*:.*/\K(.+?)$")
+DB_POSTGRESDB_USER=$(echo $DATABASE_URL | grep -o "postgres://\K(.+?):" | cut -d: -f1)
+DB_POSTGRESDB_PASSWORD=$(echo $DATABASE_URL | grep -o "postgres://.*:\K(.+?)@" | cut -d@ -f1)
+DB_POSTGRESDB_HOST=$(echo $DATABASE_URL | grep -o "postgres://.*@\K(.+?):" | cut -d: -f1)
+DB_POSTGRESDB_PORT=$(echo $DATABASE_URL | grep -o "postgres://.*@.*:\K(\d+)/" | cut -d/ -f1)
+DB_POSTGRESDB_DATABASE=$(echo $DATABASE_URL | grep -o "postgres://.*@.*:.*/\K(.+?)$")
 fi
 
 if [[ -z "${QUEUE_BULL_REDIS_HOST}" ]]; then
 # redis
-QUEUE_BULL_REDIS_PASSWORD=$(echo $REDIS_URL | grep -oP "redis://.*:\K(.+?)@" | cut -d@ -f1)
-QUEUE_BULL_REDIS_HOST=$(echo $REDIS_URL | grep -oP "redis://.*@\K(.+?):" | cut -d: -f1)
-QUEUE_BULL_REDIS_PORT=$(echo $REDIS_URL | grep -oP "redis://.*@.*:\K(\d+)/" | cut -d/ -f1)
+QUEUE_BULL_REDIS_PASSWORD=$(echo $REDIS_URL | grep -o "redis://.*:\K(.+?)@" | cut -d@ -f1)
+QUEUE_BULL_REDIS_HOST=$(echo $REDIS_URL | grep -o "redis://.*@\K(.+?):" | cut -d: -f1)
+QUEUE_BULL_REDIS_PORT=$(echo $REDIS_URL | grep -o "redis://.*@.*:\K(\d+)/" | cut -d/ -f1)
 fi
 
 # if [ -d /root/.n8n ] ; then
